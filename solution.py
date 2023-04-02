b8ebea3c3c73fd3d69a5477851723e33cbd657a4
artdem89 <82702367+artdem89@users.noreply.github.com>
2023-04-02 09:13:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import t
+from scipy.stats import norm
 
 
 chat_id = 157443210  # Ваш chat ID, не меняйте название переменной
@@ -12,7 +12,9 @@ def solution(p: float, x: np.array) -> tuple:
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
     loc = x.mean()
-    scale = x.std(ddof=1) / np.sqrt(len(x))
-    return loc - scale * t.ppf(1 - alpha / 2, df = len(x) - 1), \
-           loc + scale * t.ppf(1 - alpha / 2, df = len(x) - 1)       
+    scale = np.var(x, ddof=1) + 0.5
+    return loc - norm.ppf(1 - alpha / 2) * np.sqrt(scale / len(x)), \
+           loc + norm.ppf(1 - alpha / 2) * np.sqrt(scale / len(x))    
+    
+
     
