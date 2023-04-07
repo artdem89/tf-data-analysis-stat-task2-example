2c9303b493762da4c03f2765610e0c9c7152fc3b
artdem89 <82702367+artdem89@users.noreply.github.com>
2023-04-07 19:07:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,4 +13,4 @@ def solution(p: float, x: np.array) -> tuple:
     min = (-x).min()
     z = -np.log(1-p)/len(x)
     
-    return 2*(z-min-1/2)/(t**2), 2*(-min-1/2)/(t**2)        
+    return 2*(-min-1/2)/(t**2), 2*(z-min-1/2)/(t**2)        
