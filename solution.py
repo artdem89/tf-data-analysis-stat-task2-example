c828c3efc5137ceb3dbc91eae74b2f734ef434ed
artdem89 <82702367+artdem89@users.noreply.github.com>
2023-04-07 15:18:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
-
 
 chat_id = 157443210  # Ваш chat ID, не меняйте название переменной
 
@@ -10,14 +8,9 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    t = 38
     alpha = 1 - p
-    t_mean = np.mean(x)
-    s = np.sqrt(np.mean((2/t_mean - x / np.square(t_mean)) ** 2))
-    a = 2 / np.square(t_mean)
-    return a - norm.ppf(1 - alpha / 2) * (s / np.sqrt(len(x))), \
-           a + norm.ppf(1 - alpha / 2) * (s / np.sqrt(len(x)))    
-
-
-
-
+    min = (-x).min()
+    z = -np.log(1-p)/x.size
     
+    return 2*(-min-1/2)/(t*t), 2*(z-min-1/2)/(t*t)        
